c61533b71e7ea0fd047b9dda95f4660ac2dc6e9a
toastybridge <jtan208295@gmail.com>
2025-04-28 00:44:42 -0500

    Remove extraneous Navigator component

--- frontend/app/navigation/RootNavigator.tsx
+++ frontend/app/navigation/RootNavigator.tsx
@@ -1,21 +1,21 @@
-import { Colors } from '@/constants/Colors'
-import { useColorScheme } from '@/hooks/useColorScheme'
 import { Stack } from 'expo-router'
 import React from 'react'
 
 function RootNavigator() {
-  const colorScheme = useColorScheme();
-    const colors = Colors[colorScheme ?? 'light'];
 
   return (
     <Stack
-    screenOptions={{
-      headerShown: false
-    }}
+      screenOptions={{
+        headerShown: false
+      }}
     >
       <Stack.Screen name="screens/onboarding/Welcome" />
       <Stack.Screen name="screens/onboarding/Onboarding1" />
       <Stack.Screen name="screens/onboarding/Onboarding2" />
+
+      <Stack.Screen name="screens/tabs/Home" options={{ animation: 'none' }} />
+      <Stack.Screen name="screens/tabs/Profile" options={{ animation: 'none' }} />
+      <Stack.Screen name="screens/tabs/Settings" options={{ animation: 'none' }} />
     </Stack>
   )
 }
