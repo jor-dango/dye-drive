f955698d1a8569d55777114a3c2cf46959d85c87
toastybridge <jtan208295@gmail.com>
2025-04-28 01:14:52 -0500

    Feat: Minimal working profile/settings pages

--- frontend/app/screens/tabs/Profile.tsx
+++ frontend/app/screens/tabs/Profile.tsx
@@ -1,10 +1,93 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Animated, TouchableOpacity, useAnimatedValue } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { useRouter } from 'expo-router'
+import { HomeIcon, Settings, UserRound } from 'lucide-react-native'
+import { ProfileRoutes } from '@/constants/types';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
+
+const ANIMATION_LENGTH = 350;
 
 function Profile() {
+  const router = useRouter();
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme ?? 'light'];
+  const oppositeColors = Colors[colorScheme === 'dark' ? 'light' : 'dark']; // Note that this also catches if colorScheme is null bc null is falsy
+  const [buttonSelected, setButtonSelected] = useState<ProfileRoutes>('Profile');
+
+  const fadeOutVal = useAnimatedValue(0);
+  const marginRef = useRef(new Animated.Value(21));
+
+  // Fade in everything at the beginning
+  useEffect(() => {
+    Animated.timing(fadeOutVal, {
+      toValue: 1,
+      duration: ANIMATION_LENGTH,
+      useNativeDriver: true
+    }).start();
+  }, [])
+
+  function fadeOutTo(route: ProfileRoutes) {
+    Animated.timing(fadeOutVal, {
+      toValue: 0,
+      duration: ANIMATION_LENGTH,
+      useNativeDriver: true
+    }).start();
+
+    if (route === 'Home')
+    Animated.timing(marginRef.current, {
+      toValue: 0,
+      duration: ANIMATION_LENGTH,
+      useNativeDriver: false
+    }).start();
+    setButtonSelected(route);
+    console.log(route)
+
+    // Navigate to route after animation plays
+    setTimeout(
+      () => router.navigate(`/screens/tabs/${route}`),
+      ANIMATION_LENGTH)
+  }
+
+
   return (
-    <View className='bg-accent'>
-      <Text>Profile</Text>
+    <View className='w-full h-full flex gap-4 p-10 py-20' style={{ backgroundColor: colors.background }}>
+      <Animated.View style={{ opacity: fadeOutVal }} className={'bg-accent w-full flex-1'}>
+        <Text>Profile</Text>
+      </Animated.View>
+
+      <View className='flex flex-row w-full justify-evenly'>
+
+        <Animated.View style={{ marginTop: marginRef.current }}>
+          <TouchableOpacity
+            className='p-4 border-2 rounded-full'
+            style={{ borderColor: colors.text, backgroundColor: buttonSelected === "Profile" ? colors.text : oppositeColors.text }}
+          >
+            <UserRound stroke={!(buttonSelected === "Profile") ? colors.text : oppositeColors.text} />
+          </TouchableOpacity>
+        </Animated.View>
+
+        <View>
+          <TouchableOpacity
+            className='p-4 border-2 rounded-full mt-6'
+            style={{ borderColor: colors.text, backgroundColor: buttonSelected === "Home" ? colors.text : oppositeColors.text }}
+            onPress={() => fadeOutTo('Home')}
+          >
+            <HomeIcon stroke={!(buttonSelected === "Home") ? colors.text : oppositeColors.text} />
+          </TouchableOpacity>
+        </View>
+
+        <Animated.View style={{ marginTop: marginRef.current }}>
+          <TouchableOpacity
+            className='p-4 border-2 rounded-full'
+            style={{ borderColor: colors.text, backgroundColor: buttonSelected === "Settings" ? colors.text : oppositeColors.text }}
+            onPress={() => fadeOutTo('Settings')}
+          >
+            <Settings stroke={!(buttonSelected === "Settings") ? colors.text : oppositeColors.text} />
+          </TouchableOpacity>
+        </Animated.View>
+
+      </View>
     </View>
   )
 }
